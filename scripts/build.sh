f5413bcb048cd00c4ce84f10aefa1337495329f1
suleymanovtat <suleymanovtat@gmail.com>
2020-03-09 21:52:28 +0300

    wip

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-cd ..
-GRADLE="./gradlew"
-$GRADLE assembleRelease
-$GRADLE crashlyticsUploadDistributionRelease
-RELEASEFILE="${CIRCLE_ARTIFACTS}/cicerone-application-release-${CIRCLE_BUILD_NUM}-${CIRCLE_SHA1}.apk"
-cp ./app/build/outputs/apk/app-release.apk ${RELEASEFILE}
